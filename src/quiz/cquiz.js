b5adbb4e366367ddfba9405ba2d08aeb1a7e5ad3
saransh1528 <64313085+saransh1528@users.noreply.github.com>
2023-10-05 22:39:01 +0530

    Changing JSON file fetch link

--- src/quiz/cquiz.js
+++ src/quiz/cquiz.js
@@ -81,7 +81,8 @@ export default function Cquiz() {
   );
 
   useEffect(function () {
-    fetch("http://localhost:8000/questions")
+    //http://localhost:8000/questions
+    fetch("https://json.extendsclass.com/bin/e0d169b31fca")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataRecieved", payload: data }))
       .catch((err) => dispatch({ type: "dataFailed" }));
